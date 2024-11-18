d372e91858c25293378bf9f06c930133a746ca7a
Jose Pablo Soto Sanchez <0262205@up.edu.mx>
2024-11-18 16:00:13 -0600

    Grafica 1 casi 100% funcional

--- src/components/Grafico1.jsx
+++ src/components/Grafico1.jsx
@@ -12,55 +12,60 @@ import {
     Filler,
 } from 'chart.js';
 
-ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend,
-    Filler
-);
 
-var beneficios = [0, 56, 20, 36, 80, 40, 30, -20, 25, 30, 12, 60];
-var Años = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+function Grafico1(props) {
 
-var midata = {
-    labels: Años,
-    datasets: [
-        {
-            label: 'Beneficios',
-            data: beneficios,
-            tension: 0.5,
-            fill: true,
-            borderColor: 'rgb(80, 99, 132)',
-            backgroundColor: 'rgba(25, 99, 214, 0.5)',
-            pointRadius: 5,
-            pointBorderColor: 'rgba(8, 99, 132)',
-            pointBackgroundColor: '#7fe6f8',
+    ChartJS.register(
+        CategoryScale,
+        LinearScale,
+        PointElement,
+        LineElement,
+        Title,
+        Tooltip,
+        Legend,
+        Filler
+    );
+    
+    const labels = props.data.map(item => item.año); // Eje x: años
+    const dataValues = props.data.map(item => parseInt(item.total)); // Eje y: totales (convertidos a número)
+    console.log(labels);
+    console.log(dataValues);
+
+    
+    var midata = {
+        labels: labels,
+        datasets: [
+            {
+                label: 'Beneficios',
+                data: dataValues,
+                tension: 0.5,
+                fill: true,
+                borderColor: 'rgb(80, 99, 132)',
+                backgroundColor: 'rgba(25, 99, 214, 0.5)',
+                pointRadius: 5,
+                pointBorderColor: 'rgba(8, 99, 132)',
+                pointBackgroundColor: '#7fe6f8',
+            },
+        ],
+    };
+    
+    var misoptions = {
+        scales: {
+            x: {
+                ticks: { color: 'rgb(0,0,0)' },
+            },
         },
-        {
-            label: 'Otra línea',
-            data: [20, 25, 60, 65, 45, 10, 0, 25, 35, 7, 20, 25],
-        },
-    ],
-};
+        responsive: true, // Hace que el gráfico sea responsivo
+        maintainAspectRatio: false, // Permite que el gráfico ocupe todo el espacio disponible
+    };
+    
 
-var misoptions = {
-    scales: {
-        x: {
-            ticks: { color: 'rgb(0,0,0)' },
-        },
-    },
-    responsive: true, // Hace que el gráfico sea responsivo
-    maintainAspectRatio: false, // Permite que el gráfico ocupe todo el espacio disponible
-};
 
-export default function Grafico1() {
     return (
         <div style={{ width: '100%', height: '100%' }}>
             <Line data={midata} options={misoptions} />
         </div>
     );
 }
+
+export default Grafico1;
--- src/components/RealTime.jsx
+++ src/components/RealTime.jsx
@@ -1,13 +1,20 @@
-import React from "react"
+import React, {useState} from "react"
 import Grafico1 from "./Grafico1";
+import datos from "../datos/datos.js";
 
 function RealTime(){
+  const listaDeAñosYTotales = datos.map(item => ({
+    año: item.Año,
+    total: item.Total
+  }));
+
+  console.log(listaDeAñosYTotales);
   return (
     <div>
       <div className="justify-content-center text-center">
-        <p className=" m-2"><b>Cars in México</b> by Year</p>
+        <p className=" m-2"><b>Cars in México</b> by Year (expressed in Millions)</p>
         <div className="bg-light mx-auto px-2 border border-2 border-primary" style={{width:'80%', height:'400px'}}>
-            <Grafico1 />
+            <Grafico1 data={listaDeAñosYTotales}/>
         </div>
       </div>
     </div>
