914ab934ce80893934bc7e30ee79b8a0f2dbe8ea
MiniCubo <minicubo121@gmail.com>
2024-11-22 18:10:11 -0600

    a

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiPath =
   process.env.NODE_ENV === "production"
-    ? "https://co2-website.onrender.com/api" // Replace with your Render backend URL
+    ? "https://co2-website.onrender.com" // Replace with your Render backend URL
     : "http://localhost:5000";
 
 const instance = axios.create({
