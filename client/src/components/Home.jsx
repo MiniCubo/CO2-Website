5da7ec682400ce52f59cc4d4e94ba6e20014c893
MiniCubo <minicubo121@gmail.com>
2024-11-28 08:14:17 -0600

    css

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -4,33 +4,38 @@ import { Link } from 'react-router-dom';
 function Home() {
   return (
     <div>
-      <div>
-        <div className="Objective">
-          <h1><b>Objective</b></h1>
-          <span>
-          Our goal is to raise awareness about the alarming number of vehicles that circulate daily in Mexico, a key factor in the increase in air pollution. Through education and the promotion of sustainable alternatives such as the use of public transport, bicycles or electric vehicles, I seek to encourage a change in mobility habits that benefits both air quality and people's health. It is essential to act now to reduce our ecological footprint and ensure a cleaner future for future generations.
-          </span>
+      <div className="container px-4 py-5">
+        <div className='container'>
+          <div className="UN">
+            <h1 className="pb-2 "><b>Objective</b></h1>
+            <span className="parrafo d-block" style={{ textAlign: 'justify' }}>
+              The 2030 Agenda for Sustainable Development, adopted in September 2015 by all United Nations Member States, is a global plan aimed at eradicating poverty, protecting the planet, and ensuring prosperity for all. This ambitious framework is built around 17 Sustainable Development Goals (SDGs), addressing global challenges such as climate change, inequality, environmental degradation, and social justice. The 2030 Agenda not only focuses on economic development but also promotes an integrated approach that considers social well-being and environmental sustainability as fundamental pillars for an equitable future. As we move toward 2030, it is crucial that governments, businesses, organizations, and citizens work together to implement innovative and sustainable solutions that transform the way we live and work, ensuring that no one is left behind.
+            </span>
+          </div>
         </div>
-      </div>
+        </div>
 
-      <div className="container px-4 py-5" id="custom-cards">
-        <h2 className="pb-2 border-bottom">UN Objective</h2>
-        <div className="UN">
-          <div className="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5">
-            <div className="col">
-              <div className="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg" style={{ backgroundImage: 'url("/img/ONU-2.png")' }}>
-                <div className="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
-                  <h3 className="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Sustainable Development</h3>
-                  <ul className="d-flex list-unstyled mt-auto">
-                    <li className="d-flex align-items-center me-3"></li>
-                  </ul>
+        <div className="container px-4 py-5" id="custom-cards">
+          <h2 className="pb-2 border-bottom">UN Objective</h2>
+          <div className="UN">
+            <div className="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5">
+              <div className="col">
+                <div
+                  className="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg"
+                  style={{ backgroundImage: 'url("/img/ONU-2.png")' }}
+                >
+                  <div className="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
+                    <h3 className="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Sustainable Development</h3>
+                    <ul className="d-flex list-unstyled mt-auto">
+                      <li className="d-flex align-items-center me-3"></li>
+                    </ul>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <span className="texto" style={{ textAlign: 'justify' }}>
-            The 2030 Agenda for Sustainable Development, adopted in September 2015 by all United Nations Member States, is a global plan aimed at eradicating poverty, protecting the planet, and ensuring prosperity for all. This ambitious framework is built around 17 Sustainable Development Goals (SDGs), addressing global challenges such as climate change, inequality, environmental degradation, and social justice. The 2030 Agenda not only focuses on economic development but also promotes an integrated approach that considers social well-being and environmental sustainability as fundamental pillars for an equitable future. As we move toward 2030, it is crucial that governments, businesses, organizations, and citizens work together to implement innovative and sustainable solutions that transform the way we live and work, ensuring that no one is left behind.
-          </span>
+            <span className=" texto d-block" style={{ textAlign: 'justify' }}>
+              The 2030 Agenda for Sustainable Development, adopted in September 2015 by all United Nations Member States, is a global plan aimed at eradicating poverty, protecting the planet, and ensuring prosperity for all. This ambitious framework is built around 17 Sustainable Development Goals (SDGs), addressing global challenges such as climate change, inequality, environmental degradation, and social justice. The 2030 Agenda not only focuses on economic development but also promotes an integrated approach that considers social well-being and environmental sustainability as fundamental pillars for an equitable future. As we move toward 2030, it is crucial that governments, businesses, organizations, and citizens work together to implement innovative and sustainable solutions that transform the way we live and work, ensuring that no one is left behind.
+            </span>
         </div>
       </div>
 
